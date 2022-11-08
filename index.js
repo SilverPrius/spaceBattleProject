29e5ffc736e033891af5da3d014599644cf41297
Long Hoang <long88hoang@gmail.com>
2022-11-07 21:46:06 -0600

    Updated document

--- index.js
+++ index.js
@@ -13,15 +13,11 @@ class MainCharacter {
             console.log(`Awesome! You just hit the enemy with a ${this.firepower} damge attack. ${this.attackShout}`)
             alienObj.hull -= this.firepower
             console.log(`${alienObj.name} has ${alienObj.hull} health remaining.`)
-
         } else {
             console.log("Oh no! Where'd you learn how to shoot? You missed! 🚫🚫🚫")
-
         }
         return alienObj.hull
     }
-
-
 }
 
 const ussAssembly = new MainCharacter(20, 5, .7, 'Hey Aliens, let me know how those lasers taste! Hahahah!')
@@ -48,23 +44,34 @@ class Aliens {
     }
 }
 
-
-
 //Game object
 let game = {
 
-    retreatNow: function (playerObj) {
-        let chacesofRetreat = Math.random()
-
-        if (chacesofRetreat >= .7) {
-            console.log('Damn! Time to retreat!')
-            playerObj.hull = 20
-        } else {
-            console.log('Bring on the next one!!')
-        }
-        return
-
+    showIntro: function () {
+        console.log("Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship.\n\nBattle the aliens as you try to destroy them with your lasers.\n\nThere are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.\n\n********** To begin this mission, enter the following command into the console: game.start() **********\n\nGood luck!!")
     },
+
+    retreat: function () {
+        console.log("Engage boosters!! You were able to safely evade from the enemies 🏃💨.\n You live to fight another day. Until next time...✌️")
+    },
+
+    chooseFightorRun: function () {
+        alert('What would you like to do next??')
+        const response = prompt(`Enter 'c' to Continue Battle or 'r' to Retreat.`)
+        if (response.toLowerCase() === 'r') {
+            console.log('Engage boosters!! RETREAT!! RETREAT!! RETREAT!!\n\nYou have safely evaded the enemies')
+            alert('You have failed the mission!')
+            alert('You are dead to us!')
+            alert('Telecommunication will self destruct in 3...2....1....💥')
+            error
+        } else if (response.toLowerCase() === 'c') {
+            null
+        } else {
+            alert(`Please try again`)
+            this.chooseFightorRun()
+        }
+    },
+
     checkIfHumanWonRound: function (currentEnemy) {
         if (currentEnemy < 1) {
             console.log("They didn't stand a chance. Alien ship has been destroyed!!")
@@ -73,50 +80,34 @@ let game = {
         return false
     },
 
-    checkIfThereareAliensLeft: function (enemy) {
-        if (enemy.length < 1) {
-            console.log('Congratulations, you win! You have defeated all of the enemy ships!!')
-        } else {
-            console.log('Prepare to battle a new alien ship!')
-        }
-    },
-
-    // checkCurrentAliensHealth: function () {
-    //     return enemyLife
-    // },
-
-
-    retreat: function () {
-        console.log("That was close, but you were able to safely evade from the enemies 🏃💨. You live to fight another day! Until next time...✌️")
-    },
-
     currentAlienTarget: function (alienArr) {
         return alienArr[alienArr.length - 1]
     },
 
-
-    startGame: function () {
+    start: function () {
         let player = new MainCharacter()
         let enemy = []
         for (let i = 0; i < 6; i++) {
             enemy.push(new Aliens('Alien Ship', Math.floor(Math.random() * (6 - 3) + 3), Math.floor(Math.random() * (4 - 2) + 2), Math.random() * (.8 - .6) + .6))
         }
         let winnerOfGame; //this is the main objective of the game - to determine the winner of the game
-        while (winnerOfGame != 'Alien' || enemy.length != 0) { //as long as 'Alien' has not won the game or there are at least 1 enemy remaining
-            let currentEnemy = this.currentAlienTarget(enemy) //the current enemy target is established
+        while (winnerOfGame != 'Alien' || enemy.length != 0) {
+            let currentEnemy = this.currentAlienTarget(enemy)
+
             if (enemy.length > 0 && enemy.length < 6) {
+
                 console.log(`Here comes a new alien target 👽... It is showing a defense level of ${currentEnemy.hull}`);
             }
-            winnerOfGame = this.playRound(player, currentEnemy) // winner of the game will be either player or current enemy
+            winnerOfGame = this.playRound(player, currentEnemy)
+            enemy.pop()
             console.log(`Status Update: You have ${player.hull} health remaining and there are ${enemy.length} enemy ships left to destroy!.`)
-            enemy.pop() //the last alien ship in the enemy array will be removed and a new alien ship will become the current enemy
+            this.chooseFightorRun()
             if (winnerOfGame == 'Alien') {
                 return this.endGame(winnerOfGame)
             }
-            if (enemy.length == 0) { //if there are no remaining alien ships in the enemy array,
-                return this.endGame(winnerOfGame) //'The winner is Player' will be logged'
+            if (enemy.length == 0) {
+                return this.endGame(winnerOfGame)
             }
-            this.retreatNow(player)
         }
     },
 
@@ -126,11 +117,9 @@ let game = {
             let enemyLife = player.attack(alien)
             if (this.checkIfHumanWonRound(enemyLife)) {
                 winnerOfRound = 'Player'
-                // this.checkIfThereareAliensLeft(alien)
                 return winnerOfRound
-
             }
-            // let playerLife = -4
+
             let playerLife = alien.attack(player, alien)
 
             if (playerLife < 1) {
@@ -146,8 +135,7 @@ let game = {
         } else {
             console.log('Bummerrr.. your ship has been destroyed!! GAME OVER')
         }
-
-
     }
 }
-game.startGame()
+
+game.showIntro()
